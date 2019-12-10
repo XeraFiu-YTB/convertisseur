304d84b0fe561042d792457b1249e9b59faf0a70
XeraFiuYTB <52258801+XeraFiuYTB@users.noreply.github.com>
2019-12-10 13:19:32 +0100

    Update code.js

--- code.js
+++ code.js
@@ -93,48 +93,38 @@ function changeBinaire() {
 //////////////Esthétique////////////////
 ////////////////////////////////////////
 function matrix() {
-	var c = document.getElementById("c");
-        var ctx = c.getContext("2d");
-
-        //making the canvas full screen
-        c.height = window.innerHeight;
-        c.width = window.innerWidth;
-
-        //chinese characters - taken from the unicode charset
+	var canvasLeft = document.getElementById("left");
+        var ctxLeft = canvasLeft.getContext("2d");
+	var canvasRight = document.getElementById("right");
+        var ctxRight = canvasRight.getContext("2d");
+	
         var matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%";
-        //converting the string into an array of single characters
+	//On sépare les éléments
         matrix = matrix.split("");
+	console.log(`matrix:${matrix}`)
 
-        var font_size = 10;
-        var columns = c.width / font_size; //number of columns for the rain
-        //an array of drops - one per column
+        var font_size = 10; //
+        var columns = canvasLeft.width / font_size; //le Nombre de colonnes
         var drops = [];
-        //x below is the x coordinate
-        //1 = y co-ordinate of the drop(same for every drop initially)
         for(var x = 0; x < columns; x++)
             drops[x] = 1; 
 
-        //drawing the characters
         function draw()
         {
-            //Black BG for the canvas
-            //translucent BG to show trail
-            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-            ctx.fillRect(0, 0, c.width, c.height);
+            ctxLeft.fillStyle = "rgba(0, 0, 0, 0.04)";
+            ctxLeft.fillRect(0, 0, canvasLeft.width, canvasLeft.height);
 
-            ctx.fillStyle = "#0F0"; //green text
-            ctx.font = font_size + "px arial";
+            ctxLeft.fillStyle = "#0F0"; //vert
+            ctxLeft.font = font_size + "px arial";
             //looping over drops
             for( var i = 0; i < drops.length; i++ )
             {
-                //a random chinese character to print
                 var text = matrix[ Math.floor( Math.random() * matrix.length ) ];
-                //x = i*font_size, y = value of drops[i]*font_size
-                ctx.fillText(text, i * font_size, drops[i] * font_size);
+                ctxLeft.fillText(text, i * font_size, drops[i] * font_size);
 
                 //sending the drop back to the top randomly after it has crossed the screen
                 //adding a randomness to the reset to make the drops scattered on the Y axis
-                if( drops[i] * font_size > c.height && Math.random() > 0.975 )
+                if( drops[i] * font_size > canvasLeft.height && Math.random() > 0.975 )
                     drops[i] = 0;
 
                 //incrementing Y coordinate
