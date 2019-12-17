0b69687422dc856c647d63318becd5d3dec8b557
XeraFiuYTB <52258801+XeraFiuYTB@users.noreply.github.com>
2019-12-17 13:11:18 +0100

    Update code.js

--- code.js
+++ code.js
@@ -13,7 +13,8 @@ var newHexaDecimalValue = 0
 ////////////////////////////////////////
 
 
-function demarrage () { 
+function demarrage () {
+	
 	//Vérification PC ou Mobile
 	if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)){
 	    	document.getElementById('decimalInput').attributes["type"] = "button"
